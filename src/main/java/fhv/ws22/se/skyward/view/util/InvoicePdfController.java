9c113a2cf672cdc091371b8f10b26b6ab0767e43
AnasTaha-56 <ticoreck56@gmail.com>
2022-12-08 18:42:56 +0100

    ClassCreated

---
+++ src/main/java/fhv/ws22/se/skyward/view/util/InvoicePdfController.java
@@ -0,0 +1,11 @@
+package fhv.ws22.se.skyward.view.util;
+
+import fhv.ws22.se.skyward.view.AbstractController;
+import org.w3c.dom.*;
+
+
+public class InvoicePdfController extends AbstractController {
+    Document doc = new Document();
+
+
+}
